0d779c7d8de14209feaffcd5fb2695b2e12bcba7
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-07-13 12:59:32 +0300

    add templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './sass/main.scss';
+import ApiService from './js/apiService.js';
+import createImageCard from './templates/image-card.hbs';
+
+const refs = {
+    searchForm: document.querySelector('#search-form'),
+    galery: document.querySelector('.gallery'),
+}
+
+const apiService = new ApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    apiService.query = e.currentTarget.elements.query.value;
+    fetchImages();
+
+}
+
+function fetchImages() {
+    apiService.fetchImages().then(images => {
+        createGallery(images);
+    });
+}
+
+function createGallery(images) {
+    refs.gallery.insertAdjacentHTML('beforeend', createImageCard(images));
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,37 @@
+const API_KEY = '22470352-c65ac8cc67498f5a0ef8fcd03';
+const BASE_URL = 'https://pixabay.com/api';
+
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
